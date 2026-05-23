928f5e13215085b17234eddacf07bfdb0aa99376
Meenit Doshi <doshimeenit110503@gmail.com>
2026-05-23 19:03:57 +0530

    Fix: change room code to 4-digit numeric

--- server/server.js
+++ server/server.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 3001;
 const rooms = new Map(); // code -> game state
 
 function generateCode() {
-  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-  let code = '';
-  for (let i = 0; i < 6; i++) code += chars[Math.floor(Math.random() * chars.length)];
-  return code;
+  return String(Math.floor(1000 + Math.random() * 9000));
 }
 
 function generateId() {
